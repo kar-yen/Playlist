b8e74dd6ccab96e8565d28afe91658fb2815b823
kar-yen <kar.yap@ucdconnect.ie>
2021-07-21 19:06:02 +0800

    .h file to read list from input or file

---
+++ readList.h
@@ -0,0 +1,10 @@
+#ifndef ASSIGNMENT1_READLIST_H
+#define ASSIGNMENT1_READLIST_H
+
+int getInput(int argc, char file[]);
+int specifiedFile(char file[]);
+int searchDirectory();
+int userInput();
+int readContent(char arr[], int i, int *j);
+
+#endif //ASSIGNMENT1_READLIST_H
